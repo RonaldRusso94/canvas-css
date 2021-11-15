b2a2f1822a3d90b715d2d719014f5d41156feaff
RonaldRusso94 <RonaldRusso94@yahoo.com>
2021-11-15 08:23:23 -0500

    Created basic canvas shapes

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,59 @@
 <template>
-  <div>
-    <div>
-      <h1>Test</h1>
-      <p>TESTTTTTTT</p>
-    </div>
+  <div
+    class="
+      flex flex-col
+      justify-center
+      items-center
+      min-h-screen
+      bg-gray-400
+      relative
+    "
+  >
+    <canvas ref="canvas" width="600" height="600" class="bg-white"></canvas>
   </div>
 </template>
 
 <script>
-export default {}
+// import '../components/canvasScript.js'
+export default {
+  data() {
+    return {
+      ctx: null,
+    }
+  },
+  mounted() {
+    this.ctx = this.$refs.canvas.getContext('2d')
+    // canvas.width = 400
+    // console.log(`canvas.width`, canvas.width)
+
+    // fillRect()
+    this.ctx.fillStyle = 'red'
+    this.ctx.fillRect(20, 20, 150, 100)
+    this.ctx.fillStyle = 'blue'
+    this.ctx.fillRect(200, 20, 150, 100)
+
+    // strokeRect()
+    this.ctx.lineWidth = 5
+    this.ctx.strokeStyle = 'green'
+    this.ctx.strokeRect(100, 200, 150, 100)
+
+    // clearRect()
+    this.ctx.clearRect(25, 25, 140, 90)
+
+    // fillText()
+    this.ctx.font = '30px Arial'
+    this.ctx.fillStyle = 'purple'
+    this.ctx.fillText('Hello World!', 400, 50)
+
+    // strokeText()
+    this.ctx.lineWidth = 1
+    this.ctx.strokeStyle = 'orange'
+    this.ctx.strokeText('Hello World!', 400, 100)
+  },
+  // computed: {
+  //   canvas() {
+  //     return true
+  //   },
+  // },
+}
 </script>
