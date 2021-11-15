b938beffb8236c74661e6ee9150c0b5741d13e59
RonaldRusso94 <RonaldRusso94@yahoo.com>
2021-11-15 08:24:07 -0500

    Create vue-canvas.vue

---
+++ pages/vue-canvas.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="
+      flex flex-col
+      justify-center
+      items-center
+      min-h-screen
+      bg-gray-400
+      relative
+    "
+  >
+    <canvas ref="canvas" width="600" height="600" class="bg-white"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ctx: null,
+      circle: { x: 200, y: 200, size: 30, dx: 5, dy: 4 },
+    }
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+}
+</script>
+
+<style></style>
