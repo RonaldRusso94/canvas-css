76e772543c2df60ee7715ae170133e0485ff0a6e
RonaldRusso94 <RonaldRusso94@yahoo.com>
2021-11-15 04:17:27 -0500

    Init & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>
+      <h1>Test</h1>
+      <p>TESTTTTTTT</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
