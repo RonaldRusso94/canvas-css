ee00016332da43c3708af59fca7e0480c43fc6e9
RonaldRusso94 <RonaldRusso94@yahoo.com>
2021-11-16 16:49:21 -0500

    Animated ball on canvas w/ controls for speed & gravity

--- pages/animation.vue
+++ pages/animation.vue
@@ -8,6 +8,46 @@
       height="600"
       class="bg-white shadow-lg rounded"
     ></canvas>
+
+    <div class="w-[600px] mt-4 flex justify-around">
+      <div class="flex">
+        <h2 class="font-bold text-xl mr-2">Speed:</h2>
+        <div class="space-x-1">
+          <button
+            class="bg-gray-200 h-8 rounded px-4 font-bold"
+            @click="subSpeed"
+          >
+            -
+          </button>
+          <button
+            class="bg-gray-200 h-8 rounded px-4 font-bold"
+            @click="addSpeed"
+          >
+            +
+          </button>
+        </div>
+      </div>
+      <button class="bg-gray-200 h-8 rounded px-4 font-bold" @click="stopBall">
+        Stop
+      </button>
+      <div class="flex">
+        <h2 class="font-bold text-xl mr-2">Gravity:</h2>
+        <div class="space-x-1">
+          <button
+            class="bg-gray-200 h-8 rounded px-4 font-bold"
+            @click="subGravity"
+          >
+            -
+          </button>
+          <button
+            class="bg-gray-200 h-8 rounded px-4 font-bold"
+            @click="addGravity"
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,7 +56,7 @@ export default {
   data() {
     return {
       ctx: null,
-      circle: { x: 200, y: 200, size: 30, dx: 5, dy: 4 },
+      circle: { x: 200, y: 200, size: 30, dx: 1, dy: 1 },
     }
   },
   computed: {},
@@ -55,8 +95,57 @@ export default {
       this.circle.x += this.circle.dx
       this.circle.y += this.circle.dy
 
+      // Detect Side Walls
+      if (
+        this.circle.x + this.circle.size > this.$refs.canvas.width ||
+        this.circle.x - this.circle.size < 0
+      ) {
+        this.circle.dx *= -1
+      }
+
+      // Detect Top & Bottom walls
+      if (
+        this.circle.y + this.circle.size > this.$refs.canvas.height ||
+        this.circle.y - this.circle.size < 0
+      ) {
+        this.circle.dy *= -1
+      }
+
       requestAnimationFrame(this.update)
-      // console.log(123)
+    },
+    addSpeed() {
+      if (this.circle.dx > 0) {
+        this.circle.dx += 0.5
+      } else if (this.circle.dx < 0) {
+        this.circle.dx -= 0.5
+      } else if (this.circle.dx === 0) {
+        this.circle.dx += 0.5
+      }
+    },
+    subSpeed() {
+      if (this.circle.dx > 0) {
+        this.circle.dx -= 0.5
+      } else if (this.circle.dx < 0) {
+        this.circle.dx += 0.5
+      }
+    },
+    addGravity() {
+      if (this.circle.dy >= 0) {
+        this.circle.dy += 0.5
+      } else if (this.circle.dy < 0) {
+        this.circle.dy -= 0.5
+      }
+    },
+    subGravity() {
+      if (this.circle.dy > 0) {
+        this.circle.dy -= 0.5
+      } else if (this.circle.dy < 0) {
+        this.circle.dy += 0.5
+      }
+    },
+    stopBall() {
+      this.circle.dx = 0
+      this.circle.dy = 0
     },
   },
 }
